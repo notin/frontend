92926d64a8c15dd61a0f49188d2fc1f64d354586
chadforgit <chadforgit@gmail.com>
2018-12-30 18:49:58 -0800

    adding libraries

---
+++ public/assets/js/conversation.js
@@ -0,0 +1,32 @@
+
+function load()
+{
+    const http = new XMLHttpRequest();
+    const url='http://localhost:8080/conversation?id=null';
+    http.open('GET', url, false);
+    let value = "application/json; charset=utf-8;";
+    http.setRequestHeader('Accept', value );
+    http.setRequestHeader('Content-Type', value );
+    http.send();
+
+    let responseText = null;
+    try {
+         responseText = JSON.stringify(http.responseText);
+    }
+    catch (e) {
+
+    }
+    console.log('printing text');
+    console.log(responseText);
+    return responseText
+}
+function setText(element, load1) {
+    var elementById = document.getElementById(element);
+    console.log(load1);
+    elementById.value = load1;
+    return elementById
+}
+function show(element) {
+    var load1 = load();
+    setText(element, load1);
+}
